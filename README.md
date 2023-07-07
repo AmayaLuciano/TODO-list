6eb951a2b08405be2e77368f68b729ae4d3b7c91
Luciano Amaya <luciano.amayaaa@gmail.com>
2023-07-07 11:54:53 -0300

    push repository

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# TODO List
+
+Esta aplicacion permite agregar, completar y eliminar tareas. 
+
+Ademas quedan guardadas en el local storage, es decir, si refrezcas la pagina, las tareas no van a desaparecer.
+
+
+
+
+
+
+
+
+
+
+## Recursos utilizados
+
+- Vite JS: para crear e inicializar el proyecto.
+- Material UI: https://mui.com/
+
+
+
+
+
+## Dificultades y facilidades
+    
+Dado que mi fuerte es el front, y ademas con React, puedo decir que se me hizo bastante sencillo hacer esta aplicacion. 
+
+Algo nuevo para mi fue usar Material UI, generalmente uso Tailwind css o tambien use librerias como Chakra UI, pero como quise probar algo nuevo utilicé Material UI.
+
+Me parecio una buena funcionalidad agregar el local storage asi las tareas no se pierden, y como estas no se pierden me parecio bien tambien agregarle un boton para eliminarlas, sino quedaria una lista gigante de tareas.
+
---
+++ src/App.tsx
@@ -0,0 +1,138 @@
+import { useState, useEffect } from 'react';
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@mui/material';
+import './App.css';
+import { Delete } from '@mui/icons-material';
+
+type Todo = {
+  id: number;
+  task: string;
+  completed: boolean;
+};
+
+const getLocalItems = () => {
+  const list = localStorage.getItem('todos');
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
+function App() {
+  const [item, setItem] = useState<string>('');
+  const [items, setItems] = useState<Todo[]>(getLocalItems());
+  const [pendingTasks, setPendingTasks] = useState<number>(0);
+
+  useEffect(() => {
+    const storedItems = localStorage.getItem('todos');
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(items));
+    const count = items.filter((item) => !item.completed).length;
+    setPendingTasks(count);
+  }, [items]);
+
+  const addTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (item !== '') {
+      const newItem: Todo = {
+        id: Math.random(),
+        task: item,
+        completed: false,
+      };
+      setItems((prevItems) => [...prevItems, newItem]);
+    }
+    setItem('');
+  };
+
+  const completeTask = (id: number) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  };
+
+  const deleteTask = (id: number) => {
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom>
+        Todo List
+      </Typography>
+
+      <form
+        onSubmit={(e) => addTask(e)}
+        style={{ display: 'flex', marginBottom: '1rem' }}
+      >
+        <TextField
+          fullWidth
+          label="New Todo"
+          variant="outlined"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Add
+        </Button>
+      </form>
+
+      <Container>
+        <p>Tareas Pendientes: {pendingTasks}</p>
+      </Container>
+      <Container>
+        {items.length > 0 ? (
+          <List>
+            {items?.map((item) => (
+              <ListItem
+                key={item.id}
+                onClick={() => completeTask(item.id)}
+                style={{
+                  textDecoration: item.completed ? 'line-through' : 'none',
+                }}
+              >
+                <ListItemText primary={item.task} />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => deleteTask(item.id)}
+                  >
+                    <Delete />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        ) : (
+          <p>No tienes tareas </p>
+        )}
+      </Container>
+    </Container>
+  );
+}
+
+export default App;
